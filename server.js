cdb7b03c18d7e39a053616c84aaaaf90ecadc584
Ricky Tsao <Ricky_Tsao@emap.com>
2016-06-08 15:55:55 +0800

    1555

--- server.js
+++ server.js
@@ -1,49 +1,34 @@
-/*var http = require('http');
-
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
-*/
-
-
 var express     = require('express');
 if(express) {
-  console.log("express loaded");
+  console.log("server.js - express loaded");
 }
 
 var app         = express();
 if(app) {
-  console.log("app loaded");
+  console.log("server.js - app loaded");
 }
  
  var config 		= require('./config'); // get our config file
  if(config) {
-   console.log("config loaded");
+   console.log("server.js - config loaded");
    console.log(config.secret + ", " + config.database);
  }
 
 var bodyParser  = require('body-parser');
 if(bodyParser) {
-  console.log("body-parser loaded");
+  console.log("server.js - body-parser loaded");
 }
 //var morgan      = require('morgan');
  
+ //azure mobiel apps stuff
+ var mobileApp = require('azure-mobile-apps')();
+
 var http = require('http');
-
 var port = process.env.PORT || 1337;
-
-/*
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
-*/
 
 
 app.set('superSecret', config.secret); // secret variable
-console.log("app sets config superSecret");
+console.log("server.js - app sets config superSecret");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -56,14 +41,3 @@ console.log("app requires ./controllers");
 app.listen(port);
 console.log('server.js - web services now exposed at http://localhost:' + port);
 exports.app = app;
-
-/*
- 
-// use morgan to log requests to the console
-app.use(morgan('dev'));
- 
-app.listen(port);
-console.log('web services now exposed at http://localhost:' + port);
- 
- exports.app = app;
- */
